19a522a68479a035cd09bdabb5fa48b79f36754b
hacers <hacer.karademir@gmail.com>
2020-05-05 17:54:13 +0100

    Login.java was added

---
+++ src/tests/Login.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class Login {
+    public static void main(String[] args) {
+        String name = "Blue";
+        String password = "******";
+        System.out.println("Enter the user name: "+ name);
+        System.out.println("Enter the password" + password);
+
+    }
+}
