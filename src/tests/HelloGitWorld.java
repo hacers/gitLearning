a24d5d2cf5455a33bd064b754e4032ceaad6fd4d
hacers <hacer.karademir@gmail.com>
2020-05-06 16:11:13 +0100

    added new sentence

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -3,6 +3,7 @@ package tests;
 public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(" I wish you a good day");
     }
 
 }
